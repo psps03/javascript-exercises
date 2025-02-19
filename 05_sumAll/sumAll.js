3bc2a4ce135f456e85ee482943f0e3bf43798379
Cyrilorry <176897948+psps03@users.noreply.github.com>
2025-02-19 15:02:19 +0800

    Calculated the total sum of every integer between two numbers (inclusive)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(rangeStart, rangeEnd) {
+    let totalSum = 0;
 
+    if (rangeStart < 0 || rangeEnd < 0 || !Number.isInteger(rangeStart) || !Number.isInteger(rangeEnd)){
+        return "ERROR";
+    } else if (rangeStart < rangeEnd){
+        for (; rangeStart <= rangeEnd; rangeStart++){
+            totalSum += rangeStart;
+        }
+    } else if (rangeStart > rangeEnd){
+        for (; rangeStart >= rangeEnd; rangeStart--){
+            totalSum += rangeStart;
+        }
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
