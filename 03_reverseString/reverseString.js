dc1df1233c7a9bfa999af785525a7d31af429607
Cyrilorry <176897948+psps03@users.noreply.github.com>
2025-02-19 10:19:17 +0800

    Reversing string using for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let reversed = "";
+    for(let i = stringToReverse.length - 1; i >= 0; i--){
+        reversed += stringToReverse[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
