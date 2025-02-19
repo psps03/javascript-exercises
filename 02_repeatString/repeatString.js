32abb4b27c6334a2ca973506d17894ea289bc28d
Cyrilorry <176897948+psps03@users.noreply.github.com>
2025-02-19 10:04:29 +0800

    Answer to repeatSring exercise using for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, noOfIteration) {
+    let repeatedString = '';
+    if(noOfIteration < 0){
+        repeatedString = "ERROR";
+    } else{
+        for(; noOfIteration > 0; noOfIteration--){
+            repeatedString += stringToRepeat;
+        }
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
